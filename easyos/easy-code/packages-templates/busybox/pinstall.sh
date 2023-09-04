76d1af52308c3edc5430e8c803a769f8d5ccd531
bkauler <bkauler@gmail.com>
2023-09-04 17:37:04 +0800

    packages-templates: fixes for usr-merge

--- easyos/easy-code/packages-templates/busybox/pinstall.sh
+++ easyos/easy-code/packages-templates/busybox/pinstall.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
 if [ "`pwd`" != "/" ];then
- if [ ! -f ./usr/bin/bash ];then
-  echo '#!/usr/bin/ash
-exec /usr/bin/ash $@' > ./usr/bin/bash
-  chmod 755 ./usr/bin/bash
- else
-  #as busybox got installed second, it's 'sh' overwrote...
-  ln -snf bash ./usr/bin/sh
- fi
+ #as busybox got installed second, it's 'sh' overwrote...
+ ln -snf bash ./usr/bin/sh
 fi
