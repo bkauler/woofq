ecaff62bbda9e94b73b1553b1953534b980a9651
bkauler <bkauler@gmail.com>
2024-02-23 09:25:28 +0800

    woofv: packages-templates: inkscape fix

--- easyos/easy-code/rootfs-skeleton/usr/local/woofV/pkg-fix/packages-templates/inkscape/pinstall.sh
+++ easyos/easy-code/rootfs-skeleton/usr/local/woofV/pkg-fix/packages-templates/inkscape/pinstall.sh
@@ -9,3 +9,9 @@ if [ "`pwd`" != "/" ];then
  echo 'exec inkscape "$@"' >> ./usr/local/bin/defaultdraw
  chmod 755 ./usr/local/bin/defaultdraw
 fi
+
+#20240222 de-bloat...
+if [ -d usr/lib/libreoffice/help ];then
+ rm -rf usr/lib/libreoffice/help
+ mkdir usr/lib/libreoffice/help
+fi
