a6a7a62f6c927835d278708960971bbfbe99aef4
bkauler <bkauler@gmail.com>
2024-02-27 10:37:11 +0800

    woofv: packages-templates: script removed

--- easyos/easy-code/rootfs-skeleton/usr/local/woofV/pkg-fix/packages-templates/ncurses-libs/pinstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ -e usr/lib/libncursesw.so.6 ];then
- ln -s libncurses.so.6 usr/lib/libncurses.so.5 2>/dev/null
- ln -s libncursesw.so.6 usr/lib/libncursesw.so.5 2>/dev/null
- ln -s libtinfo.so.6 usr/lib/libtinfo.so.5 2>/dev/null
-fi
