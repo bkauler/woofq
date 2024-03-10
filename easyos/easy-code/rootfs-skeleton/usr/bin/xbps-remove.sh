7ddf41bbd34fa72ed2be6e313d3fa5a186bdc08e
bkauler <bkauler@gmail.com>
2024-03-10 09:17:45 +0800

    woofv: xbps-remove.sh uninstall fixes

--- easyos/easy-code/rootfs-skeleton/usr/bin/xbps-remove.sh
+++ easyos/easy-code/rootfs-skeleton/usr/bin/xbps-remove.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #ref: /etc/profile.d/xbps-aliases
+#20240310 uninstall fixes.
 
 mkdir -p /tmp/woofV
 . /etc/DISTRO_SPECS
@@ -30,6 +31,7 @@ if [ "$SUMii1" != "$SUMii2" ];then
    mv -f /tmp/woofV/new-uip /root/.packages/user-installed-packages
    
    #if an app has been installed to run non-root, delete the .bin and .bin0...
+   # (see also /usr/local/petget/removepreview.sh)
    grep -q 'usr/share/applications' /root/.packages/${aNVR}.files
    if [ $? -eq 0 ];then
     for aDT in $(grep '/usr/share/applications/' /root/.packages/${aNVR}.files)
@@ -41,6 +43,39 @@ if [ "$SUMii1" != "$SUMii2" ];then
       [ -f /usr/bin/${EXEC}.bin ] && rm -f /usr/bin/${EXEC}.bin
       [ -f /usr/bin/${EXEC}.bin0 ] && rm -f /usr/bin/${EXEC}.bin0
       sed -i "/^${EXEC}=/d" /root/.clients-status
+      #20240310 hide home folder...
+      if [ -d /home/${EXEC} ];then
+       if [ -d /home/.${EXEC} ];then #precaution
+        rm -rf /home/.${EXEC}
+       fi
+       mv -f /home/${EXEC} /home/.${EXEC}
+      fi
+      #20240310 remove a desktop icon if running non-root...
+      grep -q "/home/${EXEC}/${EXEC}" /root/Choices/ROX-Filer/PuppyPin
+      if [ $? -eq 0 ];then
+       echo "<?xml version=\"1.0\"?>
+<env:Envelope xmlns:env=\"http://www.w3.org/2001/12/soap-envelope\">
+ <env:Body xmlns=\"http://rox.sourceforge.net/SOAP/ROX-Filer\">
+  <PinboardRemove>
+   <Path>/home/${EXEC}/${EXEC}</Path>
+  </PinboardRemove>
+ </env:Body>
+</env:Envelope>" | rox -R
+      fi
+      #20240310 remove a desktop icon if running as root...
+      grep -q "/usr/bin/${EXEC}" /root/Choices/ROX-Filer/PuppyPin
+      if [ $? -eq 0 ];then
+       echo "<?xml version=\"1.0\"?>
+<env:Envelope xmlns:env=\"http://www.w3.org/2001/12/soap-envelope\">
+ <env:Body xmlns=\"http://rox.sourceforge.net/SOAP/ROX-Filer\">
+  <PinboardRemove>
+   <Path>/usr/bin/${EXEC}</Path>
+  </PinboardRemove>
+ </env:Body>
+</env:Envelope>" | rox -R
+      fi
+      #20240310 packages-templates may have renamed .desktop file when install...
+      rm -f /usr/share/applications/${EXEC}.desktop 2>/dev/null
      fi
      build-rox-sendto "-${aDT}"
     done
