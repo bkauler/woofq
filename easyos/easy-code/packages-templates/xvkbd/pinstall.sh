34dd19269ce89b91a70e73719c38453947883999
bkauler <bkauler@gmail.com>
2024-07-24 01:36:03 +0800

    xvkbd fix

--- easyos/easy-code/packages-templates/xvkbd/pinstall.sh
+++ easyos/easy-code/packages-templates/xvkbd/pinstall.sh
@@ -8,3 +8,7 @@ sed -i -e 's%showdesktop</TrayButton>$%showdesktop</TrayButton>\n\t\t<TrayButton
 if [ -f root/.jwm/jwmrc-personal ];then
  sed -i -e 's%</JWM>%<Group>\n<Name>xvkbd</Name>\n<Option>icon:mini-keyboard.xpm</Option>\n<Option>noborder</Option>\n<Option>nofocus</Option>\n</Group>\n</JWM>%' root/.jwm/jwmrc-personal
 fi
+
+#20240723 bringing back xvkbd builtin in easy.sfs
+#the first sed operation above will not do anything, as showdesktop tray entry doesn't get
+#created in /root/.jwmrc-tray until /root/.xinitrc calls /usr/local/ui/jwmrc-insert-extra
