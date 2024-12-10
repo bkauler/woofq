af7fac0e26f9f96664c1347ec85ce8157e630c47
bkauler <bkauler@gmail.com>
2024-12-10 09:25:29 +0800

    move flip-button from centre of screen to tray in container

---
+++ easyos/easy-code/rootfs-skeleton/usr/local/easy_containers/flip-tray.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#20241210 called from /root/.jwmrc-tray
+
+#/usr/local/easy_containers/flip-out-of-container daemon waiting for this...
+echo 1 > /.flip-out-flg
+
+###end###
--- easyos/easy-code/rootfs-skeleton/usr/local/easy_containers/flip.sh
+++ easyos/easy-code/rootfs-skeleton/usr/local/easy_containers/flip.sh
@@ -4,6 +4,17 @@
 export TEXTDOMAIN=easy-containers
 export OUTPUT_CHARSET=UTF-8
 
+#check if already moved to tray...
+pidof jwm >/dev/null
+if [ $? -eq 0 ];then
+ if [ -e /root/.jwmrc-tray ];then
+  grep -q -F 'flip-tray.sh' /root/.jwmrc-tray
+  if [ $? -eq 0 ];then
+   exit
+  fi
+ fi
+fi
+
 CR='
 '
 HELPICON="gtk-index"
@@ -29,7 +40,7 @@ class \"*\" style \"windowstuff\"
 echo "$gtkrc" > /tmp/flip_gtkrc${$}
 export GTK2_RC_FILES=/tmp/flip_gtkrc${$}:/root/.gtkrc-2.0
 
-H_flip="$(gettext 'Click button to flip back to main desktop.')${CR}$(gettext 'Or, flip with the key combination ALT+F6')${CR}$(gettext 'Note: some keyboards require FN+ALT+F6')${CR}${CR}$(gettext 'If the button is used, the clipboard content will also be copied to the main desktop clipboard.')${CR}${CR}$(gettext 'Back on the main desktop, you may flip back into the container by clicking on the desktop icon or entry in the tray. The clipboard will be copied in.')${CR}$(gettext 'The container may be killed by right-click on the tray entry and choose <b>Kill</b>')"
+H_flip="$(gettext 'Click button to flip back to main desktop.')${CR}$(gettext 'Or, flip with the key combination ALT+F6')${CR}$(gettext 'Note: some keyboards require FN+ALT+F6')${CR}${CR}$(gettext 'If the button is used, the clipboard content will also be copied to the main desktop clipboard.')${CR}${CR}$(gettext 'Click the small button at bottom-right to move flip-button from center of screen to right-side of the tray.')${CR}${CR}$(gettext 'Back on the main desktop, you may flip back into the container by clicking on the desktop icon or entry in the tray. In the former case, the clipboard will be copied in.')${CR}$(gettext 'The container may be killed by right-click on the tray entry and choose <b>Kill</b>')"
 
 M_close="$(gettext 'Close')"
 export DLG_HELP_flip="<window resizable=\"false\" title=\"$(gettext 'Help: Flip out of container')\" icon-name=\"gtk-index\" window_position=\"1\">
@@ -42,6 +53,21 @@ export DLG_HELP_flip="<window resizable=\"false\" title=\"$(gettext 'Help: Flip 
   </hbox>
  </vbox></window>"
 
+MOVEBUTTON=''
+if [ -e /root/.jwmrc-tray ];then
+ pidof jwm >/dev/null
+ if [ $? -eq 0 ];then
+  grep -q 'STARTEXTRA' /root/.jwmrc-tray
+  if [ $? -eq 0 ];then
+   MOVEBUTTON="<button>
+  <input file>/usr/local/lib/X11/mini-icons/go-jump.png</input>
+  <action>/usr/local/easy_containers/flip-move-2-tray</action>
+  <action>exit:gotout</action>
+ </button>"
+  fi
+ fi
+fi
+
 export FLIP_DLG="<window decorated=\"false\" skip_taskbar_hint=\"true\">
 <hbox>
  <button>
@@ -53,16 +79,16 @@ export FLIP_DLG="<window decorated=\"false\" skip_taskbar_hint=\"true\">
    <input file>/usr/local/lib/X11/mini-icons/mini-question.xpm</input>
    <action type=\"launch\">DLG_HELP_flip</action>
   </button>
+  ${MOVEBUTTON}
  </vbox>
 </hbox>
 </window>"
 
-gtkdialog --program=FLIP_DLG --center
+RETVALS="$(gtkdialog --program=FLIP_DLG --center)"
 
-#20241130 temp popup...
-if [ -x /usr/sbin/popup ];then
- sleep 0.2
- M1="$(gettext 'Containerized desktop')"
- popup "terminate=3 timecnt=dn name=cdpopup placement=center background=#06ee9b|<b><big><big>${M1}</big></big></b>"
+echo "${RETVALS}" | grep -q 'gotout'
+if [ $? -eq 0 ];then
+ jwm -restart
 fi
+
 ###end###
